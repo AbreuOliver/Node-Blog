baff19fedb8a774638f37f73b2ef467783967521
Oliver Abreu <oliverabreu@Olivers-MacBook.local>
2019-03-06 07:58:29 -0500

    This commit will initialize the server

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 // IMPORT EXPRESS PACKAGE
 const express = require('express');
+
+
 // CREATE THE SERVER
 const server = express();
+
+// APPLY MIDDLEWARE
+server.use(express.json());
 
 // handle requests to the root of the api, the / route
 server.get('/', (req, res) => {
   res.send('Hello, from Express!');
 });
 
-// watch for connections on port 5000
-server.listen(5000, () =>
-  console.log('Server running on http://localhost:5000')
-);
+module.exports = server;
